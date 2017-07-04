457ccee4f14a227280098722e1cf456a159e9f70
soulofocean <251455882@qq.com>
2017-07-04 17:01:56 +0800

    Add files via upload

---
+++ MainConProj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SortUtility;
+
+namespace MainConProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicSortType b = new ShellSortType();
+            b.GenArr(9,false);
+            b.DispArr();
+            b.SortArr(SortEnum.ASC);
+            b.DispArr();
+            b.SortArr(SortEnum.DESC);
+            b.DispArr();
+            b.SortArr(SortEnum.ASC);
+            b.DispArr();
+            Console.ReadLine();
+        }
+    }
+}
